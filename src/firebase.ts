cc2291486004c679fa349bf786088f0b5969a0cb
lumskii <67390011+lumskii@users.noreply.github.com>
2025-09-27 03:50:52 -0700

    fix: correct databaseURL assignment in Firebase configuration

--- src/firebase.ts
+++ src/firebase.ts
@@ -7,6 +7,7 @@ import { getAnalytics, isSupported } from 'firebase/analytics'
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASEURL,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
